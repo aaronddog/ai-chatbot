4332f9869a86bb7bedb1daa4444fe01b2b30d821
Aaron Joyce <aaron.joyce@datadoghq.com>
2025-10-07 08:59:31 +0100

    dd-trace without graphql dep

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,15 @@
 import { registerOTel } from "@vercel/otel";
 
 export function register() {
+  if (typeof window === "undefined") {
+    require('dd-trace').init({
+      llmobs: {
+        mlApp: "ai-chatbot",
+        agentlessEnabled: true,
+      },
+      site: "datadoghq.com",
+      env: "prod",
+    });
+  }
   registerOTel({ serviceName: "ai-chatbot" });
 }
